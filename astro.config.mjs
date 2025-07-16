ed7c524426a5c7edea7faae65fe8ff3332dbc5a2
penken.camera <221147054+penken-camera@users.noreply.github.com>
2025-07-17 08:41:04 +0900

    変更：公開URLにサブパスが入らないようbaseを変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    base: '/peken-camera/',
+    base: '/',
     integrations: [mdx(), tailwind()],
 });
